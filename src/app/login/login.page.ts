44d220ca773e97fc5fd06dd0589674bc817dc676
Lucas Oliver <115434062+LzLuscas@users.noreply.github.com>
2024-10-20 21:57:59 -0300

    finals

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,6 +12,7 @@ export class LoginPage {
   email: string = '';   
   password: string = ''; 
   isAuthenticated: boolean = false; 
+  userId: string | null = null; // Puxando UID do usuario, OLHA PARA CÁ QUAQUA!! Armazena o UID do usuário!
 
   constructor(
     private afAuth: AngularFireAuth, 
@@ -20,7 +21,6 @@ export class LoginPage {
     private router: Router 
   ) {}
 
-  // Função de login
   async login() {
     const loading = await this.loadingController.create({
       message: 'Carregando...',
@@ -31,6 +31,10 @@ export class LoginPage {
       const userCredential = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
       await loading.dismiss();  
       this.isAuthenticated = true; 
+      
+      this.userId = userCredential.user?.uid || null;
+      console.log('UID do usuário:', this.userId);
+
       this.showToast('Login bem sucedido'); 
 
       // Redireciona pra página Home
@@ -42,14 +46,17 @@ export class LoginPage {
     }
   }
 
-  // Função para logout
+  // Função pra logout
   async logout() {
     await this.afAuth.signOut();
     this.isAuthenticated = false;  
+    this.userId = null; // Limpa o UID quando o usuario desloga
     this.showToast('Desconectado com sucesso!');
+
+    // Redireciona para a página de login
+    this.router.navigate(['/login']); 
   }
 
-  
   async showToast(message: string) {
     const toast = await this.toastController.create({
       message,

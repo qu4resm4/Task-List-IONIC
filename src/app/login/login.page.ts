74eb3b1aa93ecb16dc3568b83cc960a311008444
Lucas Oliver <115434062+LzLuscas@users.noreply.github.com>
2024-10-20 22:15:24 -0300

    -

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -12,6 +12,7 @@ export class LoginPage {
   email: string = '';   
   password: string = ''; 
   isAuthenticated: boolean = false; 
+  userId: string | null = null; // Armazena o UID do usuário
 
   constructor(
     private afAuth: AngularFireAuth, 
@@ -31,6 +32,11 @@ export class LoginPage {
       const userCredential = await this.afAuth.signInWithEmailAndPassword(this.email, this.password);
       await loading.dismiss();  
       this.isAuthenticated = true; 
+      
+      // Obtém o UID do usuário
+      this.userId = userCredential.user?.uid || null;
+      console.log('UID do usuário:', this.userId);
+
       this.showToast('Login bem sucedido'); 
 
       // Redireciona pra página Home
@@ -42,18 +48,19 @@ export class LoginPage {
     }
   }
 
-  // Função para logout
-  async logout() {
+   // Função para logout
+   async logout() {
     await this.afAuth.signOut();
-    this.isAuthenticated = false;  
     this.showToast('Desconectado com sucesso!');
+
+    // Redireciona para a tela de login
+    this.router.navigate(['/login']);
   }
 
-  
   async showToast(message: string) {
     const toast = await this.toastController.create({
       message,
-      duration: 2000 
+      duration: 2000
     });
     toast.present();
   }
